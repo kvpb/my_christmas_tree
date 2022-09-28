c1b53ca93ee0162a37b38d99041384520426762a
Karl <local-part@domain>
2022-09-29 01:41:20 +0200

    my_christmas_tree/ updated: finished My Christmas Tree.

---
+++ main.c
@@ -0,0 +1,240 @@
+/*//	2022.08.25@22:00:00  
+////
+////	Hold on a second. That ban was bullshit. I quickly came to laugh it off. But I didn't get it off my mind as fast. Something about Hotz kinda freaked me out this time, more than the first time.
+////	The guy likes whining. He even admitted it. I called him out on his hypocrisy the last time. He couldn't argue back. He behaved childishly. He talked childishly. Fuck, I mean, he argued childishly. Sounds like somebody got bullied at school a bit too hard. The guy is a paranoid lunatic. He may not be a full-blown psychopath, but he's definitely a frustrated retard. They left San Francisco for financial reasons? My ass. For social reasons, at best.
+////	He had said publicly he wanted to live in a world of 'AI', that is anything labeled AI by marketing hypebeasts for consumerist ~~bugmen~~ salarymen, and bang starlets in a VR headset. Gauge that level of self-awareness! That made me cringe hard as fuck. But everyone has their quirks. Yet working at Comma.AI on OpenPilot would have benefited me anyway, so I didn't say anything. Since the guy turned out to be a degenerate, I don't care anymore.
+////	But something kept on bugging me. Why the fuck did the mongo ban me? Luke confirmed it was Hotz who did. Why? I didn't say anything reprehensible. I got my own theory on it, and I'll leave it at that: Hotz is an insecure, hysterical, vicious, perverse little asshole.
+////
+////	When you read me, you can't think I was encouraging to go at Nix. There's just no way. Or else you're even worse than that idiot, like brain-damaged worse. I said people like Nix are leeches. They are. I expressed wondering how much taxpayer money gets wasted on such dogshit. A lot. When Hotz's second-in-command went AmErIcA bAd CaPiTaLiSm bAd OrAnGe MaN bAd InSeRt SoMeThInG nOt EvEn ReMoTeLy RiGhT-wInG hErE bAd, I replied we have those in Europe too. We do. I said Hotz should avoid talking about the lawsuit this way. He should. [He really, really should, and he should at least reword about 5 bits of his open letter, but he's a moron, so hopes aren't high.] When some ill-informed kid started talking about the case, I just explained him how things can't go wrong just like that, and I mentioned no one should attack Nix, especially on Comma.AI's behalf, [as if they actively represented Comma.AI,] because this is between Hotz, Comma.AI and Nix, this is Hotz's and Comma.AI's business, not anyone else's. Hotz replied 'wtf don't say stuff like that' or some shit within a minute. The other sperg called me 'a mole lawyer', and someone instantly, in less than a second, banned me. I vividly recall that shit.
+////	Remember she told us again and again it always was about frequency and quality, latency and reactivity, rather than quantity? It does seem to apply the fuck out of it here. IT'S TIME FOR ONE OF TOM WEST'S INVESTIGATIONS! It was 6:00:00 AM here, so I guess it only was 9:00:00 PM in San Diego. So he likely didn't need to sleep anytime soon. The guy actively was following the Discord. He was interacting with people like never before. Less than 4 people were talking about it at that moment. He can't have misunderstood it, or else the kiddo really is a cretin worse than the other guy something28. And if he was that much of an idiot, he prolly couldn't've hacked the PlayStation 3. Unless he's an idiot savant, which isn't much better. And he didn't revert his ban or apologize for his behavior. The nail in the coffin was his timing. He timed himself replying to me. I have seen it a few times in my life. Bryan Patrick Michel Frimin, wink wink, nudge nudge, you sidewalk dogshit. I may be wrong about that. But I can sense that shit from fucking orbit, and history proved me right a lot of times on that kind of stuff. Hotz fakes to misunderstand, because he attempts to make me look like I am implying his fangirls should mess Nix up. He theatrically dramatizes it through his reaction towards me. And he implicitly bigs himself up that way. Do you see where this is going? Have his parents considered birth control? Three decades too late, but better late than never.
+////
+////	So what do I think then? I guess Hotz secretely hoped his fanboys actually would fuck Nix up one way or another eventually. When I simply mentioned people shouldn't _especially on Comma.AI's behalf_, his little fantasies started to crumble away. He felt he had to say something. He then waited for me to justify him banning me. He couldn't get anything, and so when some random fuck smeared me, he deemed it was enough, since if he had banned me right away, he would have blown himself publicly. It only was petty revenge from a self-offended spoiled brat over made-up crap. Really poor. Must try harder.
+////	I may be wrong. I can be wrong. But I doubt it. I seriously doubt it. I have seen that too many times in my life. Called it, even. This little scene of his just followed a classic script. That shit wouldn't cut it among normal people in real life. That asshole wouldn't last 5 fucking seconds out of that virtual safe space of his. He should pay a hooker instead of buying shitty virtual reality gaming headsets. Imagine being over 30 physically and not getting past 10 mentally. Get laid, and get lost.
+////
+////	I do not care in the end anyway. I am a biker. I mostly drive motorcycles, you know, actual motorized vehicles, not cages for insecure employees of the month.
+////	That last one was sarcasm. I feel I have to ~~say that out loud~~ write it down on paper just in case, because it seems a lot of vegetative dimwits and paranoid lunatics somehow happen to read human language. Literacy is not intelligence. A lot of literate people happen to prove it quite often.
+*///	Heh. Just remembered something. There goes the embodiment of hope! <X'D Bloody hell, it's almost midnight. I gotta go. Yeah. Fuck these guys! :D
+
+#include <unistd.h> // Write
+#include <stdlib.h> // STRTOL
+#include <math.h> // POW, Floor
+#include <stdio.h> // PRINTF
+
+//[ -e ./0 ] && rm 0; gcc -o 0 main.c && for i in {0..9}; do ./0 ${i} | wc -l; done ; rm 0;
+//for i in {0..9}; do ./qwasar_my_christmas_tree ${i} | wc -l; done
+//for i in {0..9}; do ./qwasar_my_christmas_tree ${i} | tail -$(( i + 1 )) | head -1 | wc -c; done
+//for i in {0..9}; do python -c "import sys; print( len(sys.argv[1]) );" "$(./qwasar_my_christmas_tree ${i} | tail -$(( i + 1 )) | head -1)"; done
+//
+//./qwasar_my_christmas_tree 4 | wc -l // 26
+//./qwasar_my_christmas_tree 5 | wc -l // 35
+//./qwasar_my_christmas_tree 3 | wc -l // 18
+//./qwasar_my_christmas_tree 2 | wc -l // 11
+//./qwasar_my_christmas_tree 1 | wc -l // 5
+//./qwasar_my_christmas_tree 0 | wc -l // 0
+//./qwasar_my_christmas_tree 5 | wc -L // 39
+//ae 1 + \( 35 - 5 \) \* 2 // 61
+//ae 61 - 39 // 22 // gotta find what gives 22 from a number of 5 and a height of 30
+//printf '\033[2J\033[3J\033[1;1H' && for i in {0..9}; do echo "n = ${i}"; ./qwasar_my_christmas_tree ${i}; done && for i in {0..9}; do s=$(./qwasar_my_christmas_tree ${i}); printf "%s" "h_${i} = "; echo "${s}" | wc -l; printf "%s" "w_${i} = "; echo "${s}" | wc -L; done
+//for i in {0..9}; do [ -e ./0 ] && rm 0; gcc -o 0 main.c && ./0 ${i}; rm 0; done
+//ae 87 - 79; ae 73 - 77; ae 61 - 61; ae 49 - 57; ae 39 - 43; ae 29 - 37; ae 21 - 25; ae 13 - 17; ae 7 - 7; ae 0 - -1 // 8 -4 0 -8 -4 -8 -4 -4 0 ~~1~~ ->  0 -4 -4 -8 -4 -8  0 -4  8 // Qwasar's `./qwasar_my_christmas_tree ${i}` VS (or rather minus) KVPB's `w -= floor( ( n - 1 ) / 2 ) * ( n - 1 ) * 2`
+
+/*int unit( int x )
+{
+	return 0 < x ? 1 : 0;
+}*/ // KVPB's piecewise Heaviside (unit) step function
+
+/*void draw( int number, int width, int height )
+{
+	// calculate the width of the line ( i * ' ' + n * '*' + '\n' )  width_branch = width_max - width_air
+	// allocate memory for the line
+	// bufferize the line; concatenate width_air / 2 ' ', width_branch '*' and '\n' to that buffer
+	// write the buffer  int FD = 1; int B = malloc( width_max - ( width_air / 2 ) + 1 ); int l = strlen(B); write( FD, B, l );
+	// free the memory of the line
+	// repeat, until i == h
+	char* B;
+	int l;
+
+	
+}*/
+
+//int main( int c_a, char* v_a[] )
+//{
+//	if ( c_a != 2 )
+//		return 1;
+//
+//	int n;
+//	int w;
+//	int h;
+//	//int w_min, h_min;
+//	int i;
+//	//int j, k;
+//
+//	n = (int) strtol( v_a[ 1 ], (char**) NULL, 10 );
+//	printf("PRINTF:\tn = %d\n", n );
+//	if ( 0 < n )
+//	{
+//		h = n * 4; // crown
+//		h += ( ( n - 1 ) * ( ( n - 1 ) + 1 ) ) / 2; //+ 1; //h += ( pow( ( n - 1 ), 2 ) + ( n - 1 ) ) / 2 + 1; // limbs // yes yes YES IT WORKS HELL YEAH I OWN YOU MOTHERFUCKER
+//		h += n; // trunk
+//	//	h = ( n * 4 ) /* treetop */ + ( ( ( n - 1 ) * ( ( n - 1 ) + 1 ) ) / 2 ) /* crown */ + n /* trunk */;
+//	}
+//	else
+//		h = 1;
+//	//h = ( ( n - 1 ) * 4 + ( ( pow( n, 2 ) + n ) / 2 ) ) /* crown height */  + n /* trunk height */; // no it succs something does not add up correctly lemme try again
+//	//printf("PRINTF:\th = %d\n", h );
+//	if ( 0 < n )
+//		w = ( 2 * ( 3 * n ) + 2 * ( n / 2 ) * ( ( n / 2 ) - ( ( n + 1 ) % 2 ) ) ) + ( 1 ); // cannot factor that shit no matter what. I guess I would need to floor a bunch of stuff every now and then. Fuck it.
+//	else
+//		w = 0;
+//	//w = 2 * ( ( 3 * n ) + ( n / 2 ) * ( ( n / 2 ) - ( ( n + 1 ) % 2 ) ) ) /* double half width */ + ( 1 ) /* center width */; // Ian Beauregard's w. Ian obviously forwent the height in the calculation of the width. Why did I not do that all along? I should have! Christ. That will teach me. I gotta refactor it at least.
+//	//w = 1; // treetop
+//	//w += ( ( h - n ) /* crown minus trunk */ - 1 ) /* minus treetop */ * 2 /* minimum widening rate */; // limbs
+//	//w -= floor( ( n - 1 ) / 2 ) * ( n - 1 ) * 2  - ( n % 2 ) * 8  + ( 1 - ( n % 2 ) ) * 4; // still sucks. Forget about it. //w -= floor( ( n - 1 ) / 2 ) * ( n - 1 ) * 2    +  ( n % 2 ) * ( -1 )  * ( 1 - ( n % 2 ) ) * 2  * ( 1 - ( n % 2 ) ) * 2  * ( 1 - ( n % 2 ) ) * 2; // Fuck it. //w -= floor( ( n - 1 ) / 2 ) * ( n - 1 ) * 2  + ( 1 - ( n % 2 ) ) * 2 * ( 1 - ( n % 2 ) ) * 4  - ( n % 2 ) * 8; // neither. //w -= ( 1 - ( n % 2 ) ) * 4 + floor( ( n - 1 ) / 2 ) * ( n - 1 ) * 2; // better. //w -= floor( ( n - 1 ) / 2 ) * n * 2; // only works for n = 1, n = 5.
+//	//w = 1 + ( h - n - 1 ) * 2  - ( n - 1 ) * 2; // because the width [of the tree] depends on the height [of the crown]. And I may need to 'conditionalize' my formula anyway. Could using a modulo operation or the unit step function help me with this?
+//	//printf("PRINTF:\tw = %d\n", w );
+//	i = 0;
+//	//printf("PRINTF:\ti = %d\n", i );
+//
+//	/*i += 1;
+//	while ( i < h )
+//	{
+//		printf("PRINTF:\t%d\t|\n", i );
+//		i++;
+//	}
+//	i = 0 + 1;
+//	while ( i < w + 1 )
+//	{
+//		printf("PRINTF:\t%d\t*\n", i );
+//		i++;
+//	}*/
+//	printf("PRINTF:\th_%d = %d\n", n, h );
+//	printf("PRINTF:\tw_%d = %d\n", n, w );
+//
+//	i = 0;
+//	while ( i < h - n )
+//	{
+//		j = 0;
+//		while ( j < w )
+//		{
+//			write( 1, "*", 1 );
+//			j++;
+//		}
+//		write( 1, "\n", 1 );
+//		i++;
+//	}
+//	i = 0;
+//	while ( i < n )
+//	{
+//		j = 0;
+//		while ( j < ( w - n ) / 2 )
+//		{
+//			write( 1, " ", 1 );
+//			j++;
+//		}
+//		j = 0;
+//		while ( j < n )
+//		{
+//			write( 1, "|", 1 );
+//			j++;
+//		}
+//		write( 1, "\n", 1 );
+//		i++;
+//	}
+//	return 0;
+//}
+int main( int c_a, char* v_a[] )
+{
+	if ( c_a != 2 )
+		return 1;
+
+	int n;
+	int w, h;
+	int number, number_backup, width_max, width, width_minus, height_max, height, index, index_offset;
+	int i, j, k;
+
+	n = (int) strtol( v_a[ 1 ], (char**) NULL, 10 );
+	if ( 0 < n )
+		h = ( n * 4 ) /* treetop */ + ( ( ( n - 1 ) * ( ( n - 1 ) + 1 ) ) / 2 ) /* crown */ + n /* trunk */; // Karl V. P. B.'s h.
+	else
+		h = 1;
+	if ( 0 < n )
+		w = ( 2 * ( 3 * n ) + 2 * ( n / 2 ) * ( ( n / 2 ) - ( ( n + 1 ) % 2 ) ) ) + ( 1 ); // Ian Beauregard's w.
+	else
+		w = 0;
+
+
+
+	//   P A R T   1 :   CROWN
+	number = n;
+	number_backup = number;
+	width_max = w;
+	width = 1;
+	width_minus = 2;
+	height_max = h;
+	height = 4;
+	index = 0;
+	index_offset = 0;
+	k = 0;
+	while ( k < n )
+	{
+		index = index_offset;
+		j = 0;
+		while ( j < height )
+		{
+			i = 0;
+			while ( i < width_max / 2 - width / 2 + 1 * 2 ) //while ( i < width_max / 2 + 1 - width + index ) // Jesus fucking Christ. This line literally took me two months. I tried so many things. I went way too far.
+			{
+				write( 1, " ", 1 );
+				i++;
+			}
+			i = 0;
+			while ( i < width )
+			{
+				write( 1, "*", 1 );
+				i++;
+			}
+			write( 1, "\n", 1 );
+			width += 2; // -,
+			index++; //     |
+			j++; //         |
+		} //                |
+		width -= 2; // <----'
+		height += 1;
+
+		width = width - width_minus;
+		index_offset += width_minus; // It does not work. The moment I decided to tinker with an offset, I knew I was going into a dead end.
+		number += 1;
+		if ( ( 3 < n ) && ( number_backup == number - 2 ) )
+		{
+			width_minus += 2;
+			number_backup = number; //+= 2;
+		}
+
+		k++;
+	}
+	// It works like shit, but... I'll optimize that part later. I'd bufferize and feed Write each each line. That oughta spare me from nesting while loops.
+
+
+
+	//   P A R T   2 :   TRUNK
+	i = 0;
+	while ( i < n )
+	{
+		j = 0;
+		while ( j < w / 2 ) //while ( j < ( w - n + 1 ) / 2 )
+		{
+			write( 1, " ", 1 );
+			j++;
+		}
+		j = 0;
+		while ( j < n )
+		{
+			write( 1, "|", 1 );
+			j++;
+		}
+		write( 1, "\n", 1 );
+		i++;
+	}
+
+	return 0;
+}
